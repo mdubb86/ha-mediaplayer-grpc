9dad68aee18944472de2a20dc36577635b7241a9
Michael Waage <mwaage@supplyframe.com>
2020-09-18 15:35:54 -0500

    fixed duplicate text in _grpc.py + bumped version

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ha-mediaplayer-grpc",
-    version="0.1.4",
+    version="0.1.6",
     packages=["mediaplayer"],
     url="https://github.com/mdubb86/ha-mediaplayer-grpc",
     description="Generated GRPC client/server code for implementing a Home Assistant Media Player integration",
